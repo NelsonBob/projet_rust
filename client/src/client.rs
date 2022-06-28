69f7f81613c64f599efc32765991ffdec4cf9b0b
lucasangoston <lucasangostonpro@gmail.com>
2022-06-28 18:20:03 +0200

    refactoring variables

--- client/src/client.rs
+++ client/src/client.rs
@@ -6,25 +6,29 @@ fn main() {
     let stream = std::net::TcpStream::connect("127.0.0.1:7878");
     match stream {
         Ok(mut stream ) => {
-            let message = "Hello";
-            let serialized = serde_json::to_string(&message);
-            let serialized = serialized.unwrap();
-            let ch = (serialized.len()) as u32;
-            stream.write_all(&ch.to_be_bytes()).unwrap();
-            stream.write_all(&serialized.as_bytes()).unwrap();
 
-            let mut v = [0; 4];
+            let message_to_send = "Hello";
 
-            stream.read( &mut v).unwrap();
+            let message_to_serialized = serde_json::to_string(&message_to_send);
+            let message_to_serialized = message_to_serialized.unwrap();
+            let serialized_message_length_to_u32= (message_to_serialized.len()) as u32;
 
-            let back_to_u32: u32 = u32::from_be_bytes(v);
-            let back_to_u32= back_to_u32 as usize;
+            stream.write_all(&serialized_message_length_to_u32.to_be_bytes()).unwrap();
 
-            let mut a = vec![0; back_to_u32];
+            stream.write_all(&message_to_serialized.as_bytes()).unwrap();
 
-            stream.read(&mut a).unwrap();
-            let finale = std::str::from_utf8(&*a).unwrap().to_string();
-            println!("{}", finale);
+            let mut array = [0; 4];
+
+            stream.read( &mut array).unwrap();
+
+            let size_message: u32 = u32::from_be_bytes(array);
+            let size_message= size_message as usize;
+
+            let mut vector = vec![0; size_message];
+
+            stream.read(&mut vector).unwrap();
+            let message_received = std::str::from_utf8(&*vector).unwrap().to_string();
+            println!("{}", message_received);
         }
         Err(err) => panic!("Cannot connect: {err}")
     }
