5b435acf3f68d471ad376a433acd239c409ab3a0
Nelson FOMEKONG <nels.fomekong@gmail.com>
2022-06-28 18:34:48 +0200

    send and receive

--- client/src/client.rs
+++ client/src/client.rs
@@ -8,13 +8,8 @@ fn main() {
         Ok(mut stream ) => {
 
             let message_to_send = "Hello";
-
             send(&mut stream, &message_to_send);
-
             let mut array = [0; 4];
-
-            stream.read( &mut array).unwrap();
-
             receive(&mut stream, array);
         }
         Err(err) => panic!("Cannot connect: {err}")
@@ -23,11 +18,10 @@ fn main() {
 }
 
 fn receive(stream: &mut TcpStream, mut array: [u8; 4]) {
+    stream.read( &mut array).unwrap();
     let size_message: u32 = u32::from_be_bytes(array);
     let size_message = size_message as usize;
-
     let mut vector = vec![0; size_message];
-
     stream.read(&mut vector).unwrap();
     let message_received = std::str::from_utf8(&*vector).unwrap().to_string();
     println!("{}", message_received);
@@ -37,8 +31,6 @@ fn send(stream: &mut TcpStream, message_to_send: &str) {
     let message_to_serialized = serde_json::to_string(&message_to_send);
     let message_to_serialized = message_to_serialized.unwrap();
     let serialized_message_length_to_u32 = (message_to_serialized.len()) as u32;
-
     stream.write_all(&serialized_message_length_to_u32.to_be_bytes()).unwrap();
-
     stream.write_all(&message_to_serialized.as_bytes()).unwrap();
 }
